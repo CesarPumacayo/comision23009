d41a27dd48c25ae6f7fa26d6a112d97a2f5cdff4
cesar <cesarbjh@gmail.com>
2023-04-19 19:34:18 -0300

    Primera variable

---
+++ clase 13/app.js
@@ -0,0 +1,49 @@
+
+
+// primera funcion que hacemos en JS
+
+
+console.log("Hola mundoo!! DESDE UN ARCHIVO EXTERNO")
+console.log("Hola mundoo!! DESDE UN ARCHIVO EXTERNO! 2")
+
+
+
+/*
+comentario
+Declaracion de variables
+*/
+console.log("La nueva variable resultado es igual a: ", resultado)
+var nombre= prompt("Ingrese su nombre:");
+document.write("El nombre ingresado es ", nombre)
+
+let numero = 5+10;
+var nombre = "Arturo";
+let booleano = true;
+const pi = 3.1416;
+var x;
+
+
+resultado= nombre+numero;
+nombre = 20;
+console.log(numero);
+console.log(nombre);
+console.log(booleano);
+
+console.log(typeof resultado);
+console.log(typeof x)
+console.log(typeof nombre);
+
+console.log(typeof numero);
+
+console.log(nombre);
+console.log(pi);
+console.log(x)
+
+
+
+
+
+
+
+
+
